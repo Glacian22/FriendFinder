74ec742ac26704fddd3d5002372378bd93603f0a
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-04-06 16:54:32 -0700

    heroku mysql calls fully functional

--- app/data/friendsDB.js
+++ app/data/friendsDB.js
@@ -18,3 +18,30 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
 });
+
+//INSERT INTO friends (name, photo, questions) VALUES (
+// 'Jeff',
+// 'https://www.bobswatches.com/rolex-blog/wp-content/uploads/2018/07/Cartier-Tank-Photo-by-Michael-J-Fox-823x1024.png', 
+// '[2, 3, 2, 3, 1]'
+// )
+var uploadSurvey = function (survey) {
+
+    var query = connection.query(
+        "INSERT INTO friends (name, photo, questions) VALUES (?, ?, ?)", [survey.name, survey.photo, survey.questions], function (err, result) {
+            if (err) throw err;
+        })
+}
+
+var getFriendList = function (res) {
+    var query = connection.query(
+        "SELECT * FROM friends", function (err, result) {
+            console.log("getting list");
+            if (err) throw err;
+            res.json(result);
+        })
+    }
+
+module.exports = {
+            uploadSurvey: uploadSurvey,
+            getFriendList: getFriendList
+        }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,30 @@
-var path = require("path");
 var friends = require("./../data/friends")
+var dbMethods = require("./../data/friendsDB")
 
-
+var uploadSurvey = dbMethods.uploadSurvey;
+var getFriendList = dbMethods.getFriendList;
 // ===============================================================================
 // API ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
-        res.json(friends);
-      });
+    app.get("/api/friends", function (req, res) {
+        //calls imported database function to retrieve full db list, passes in res so getFriendList can respond to initial API hit
+        getFriendList(res);
+    });
 
-      app.post("/api/friends", function(req, res){
-          console.log("Incoming Form Data:", req.body)
-          friends.push(req.body)
-          res.redirect(302, "/")
-      })
+    // Get survey form info
+    app.post("/api/friends", function (req, res) {
+        console.log("Incoming Form Data:", req.body)
+
+        //convert survey to db obj form
+        var dbObj = {};
+        dbObj.name = req.body.name;
+        dbObj.photo = req.body.photo;
+        dbObj.questions = JSON.stringify([parseInt(req.body.breakfast), parseInt(req.body.game), parseInt(req.body.flavor), parseInt(req.body.kink), parseInt(req.body.exercise)])
+        uploadSurvey(dbObj)
+        res.redirect(302, "/")
+    })
 
 }
