848a3f1f18ce4a474dd6ff023d86802f205f987b
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-04-06 14:01:29 -0700

    wip db

--- server.js
+++ server.js
@@ -2,26 +2,6 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-let mysql = require("mysql")
-
-// Sets up MySQL DB connection
-// heroku ClearDB info: 
-// mysql://b7a7fdebcdad14:f617b28e@us-cdbr-iron-east-03.cleardb.net/heroku_767e299fefd1ce5?reconnect=true
-var connection = mysql.createConnection({
-    host: "us-cdbr-iron-east-03.cleardb.net",
-
-    // Your username
-    user: "b7a7fdebcdad14",
-
-    // Your password
-    password: "f617b28e",
-    database: "heroku_767e299fefd1ce5"
-});
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-});
 
 // Sets up the Express App
 // =============================================================
